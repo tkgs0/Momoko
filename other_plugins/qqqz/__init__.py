cd115b51ab7f9f7c56a331bf5329a54659b747ee
tkgs0 <hgc.sakuya@ya.ru>
2022-11-12 16:17:59 +0800

    commit

---
+++ other_plugins/qqqz/__init__.py
@@ -0,0 +1,56 @@
+from nonebot.adapters.onebot.v11 import Message, MessageEvent
+from nonebot.params import CommandArg
+from nonebot.plugin import on_command
+import requests
+
+
+
+url = 'http://tfkapi.top/API/qqqz.php?qq='
+
+qqqz = on_command('查权重', priority=5, block=True)
+
+@qqqz.handle()
+async def _(event: MessageEvent, arg: Message = CommandArg()):
+    msg = _check(event, arg)
+    await qqqz.finish(msg)
+
+
+
+def _check(event, arg):
+    msg = event.get_message()
+    msg1 = arg.extract_plain_text()
+
+    if msg1 == '自己':
+        res = requests.post(url=f'{url}{event.user_id}').text
+        return res
+
+    uids = [at.data["qq"] for at in msg["at"]]
+
+    if not uids:
+        uids = msg1.strip().split()
+        if not uids:
+            return "用法:\n查权重 qq qq1 qq2 ..."
+
+    for uid in uids:
+        if not is_number(uid):
+            return '格式错误, id必须为数字'
+
+    uids = list(set(uids))
+    res = [uid+requests.post(url=f'{url}{uid}').text for uid in uids]
+    return '\n'.join(res)
+
+
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
