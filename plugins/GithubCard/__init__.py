04ff852e9b0d2dd2a66a5c20c985258076c8fd0d
tkgs0 <hgc.sakuya@ya.ru>
2022-11-13 20:35:36 +0800

    :speech_balloon:

--- plugins/GithubCard/__init__.py
+++ plugins/GithubCard/__init__.py
@@ -20,5 +20,5 @@ async def github_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
     if re.match("https://github.com/.*?/.*?", url) != None:
         imageUrl = await get_github_reposity_information(url)
         assert(imageUrl != "获取信息失败")
-        await github.finish(Message(f"[CQ:image,file={imageUrl}]"))
-        # await github.finish(Message(f"[CQ:image,file=https://image.thum.io/get/width/1280/crop/1440/viewportWidth/1280/png/noanimate/{url}]"))
+        await github.send(Message(f"[CQ:image,file={imageUrl}]"))
+        await github.finish(Message(f"[CQ:image,file=https://image.thum.io/get/width/1280/crop/1440/viewportWidth/1280/png/noanimate/{url}]"))
