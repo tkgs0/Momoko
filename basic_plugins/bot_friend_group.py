a7c3c183c65e07ee14064adafc60a379e94f4fd6
tkgs0 <hgc.sakuya@ya.ru>
2022-11-17 21:10:20 +0800

    :sparkles:新增一键退群功能

--- basic_plugins/bot_friend_group.py
+++ basic_plugins/bot_friend_group.py
@@ -1,7 +1,29 @@
-from nonebot import on_command
+from nonebot import logger, on_command
 from nonebot.rule import to_me
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    Message,
+    MessageEvent,
+    GroupMessageEvent
+)
+
+
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
 
 
 
@@ -10,7 +32,7 @@ cls_group = on_command(
 )
 
 @cls_group.handle()
-async def _cls_group(bot: Bot):
+async def _(bot: Bot):
     gl = await bot.get_group_list()
     msg = ["{group_id} {group_name}".format_map(g) for g in gl]
     msg = "\n".join(msg)
@@ -24,7 +46,7 @@ cls_friend = on_command(
 )
 
 @cls_friend.handle()
-async def _cls_friend(bot: Bot):
+async def _(bot: Bot):
     gl = await bot.get_friend_list()
     msg = ["{user_id} {nickname}".format_map(g) for g in gl]
     msg = "\n".join(msg)
@@ -33,12 +55,47 @@ async def _cls_friend(bot: Bot):
 
 
 
+del_group = on_command("退群", rule=to_me(), permission=SUPERUSER, priority=1, block=True)
+
+@del_group.handle()
+async def _(event: MessageEvent, bot: Bot, arg: Message = CommandArg()):
+    gids = arg.extract_plain_text().strip().split()
+    if gids:
+        for gid in gids:
+            if not is_number(gid):
+                await del_group.finish(f"群号必须为数字", at_sender=True)
+        msg = ''
+        for gid in gids:
+            try:
+                await bot.set_group_leave(group_id=int(gid))
+                logger.info(f"退出群聊 {gid} 成功")
+                msg += f"\n退出群聊 {gid} 成功"
+            except Exception as e:
+                logger.info(f"退出群聊 {gid} 失败 e:{e}")
+                msg += f"\n退出群聊 {gid} 失败 e:{e}"
+        await del_group.finish(msg, at_sender=True)
+
+    elif isinstance(event, GroupMessageEvent):
+        group_id = event.group_id
+        try:
+            await bot.set_group_leave(group_id=group_id)
+            logger.info(f"退出群聊 {group_id} 成功")
+            await bot.send_private_msg(user_id=event.user_id, message=f"退出群聊 {group_id} 成功")
+        except Exception as e:
+            logger.info(f"退出群聊 {group_id} 失败 e:{e}")
+            await del_group.finish(f"退出群聊失败 e:{e}", at_sender=True)
+
+    else:
+        await del_group.finish(f"需要群号", at_sender=True)
+
+
+
 group_user_list = on_command(
     "拉取群成员列表", rule=to_me(), permission=SUPERUSER, priority=1, block=True
 )
 
 @group_user_list.handle()
-async def _group_user_list(bot: Bot, event: GroupMessageEvent):
+async def _(bot: Bot, event: GroupMessageEvent):
     group_id = event.group_id
     user_list = await bot.get_group_member_list(group_id=group_id)
     user_list = "\n".join(str(user["user_id"]) for user in user_list)
