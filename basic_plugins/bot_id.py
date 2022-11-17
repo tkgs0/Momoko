57677c7481fa5b93a1e3af9f9799f592ed201cdf
tkgs0 <hgc.sakuya@ya.ru>
2022-11-18 00:34:56 +0800

    :wrench:

--- basic_plugins/bot_id.py
+++ basic_plugins/bot_id.py
@@ -22,9 +22,21 @@ start_metaevent = on_metaevent(rule=check_first_connect, temp=True)
 async def start(bot: Bot) -> None:
     user_id = await bot.get_login_info()
     user_id = json.dumps(user_id)
+
     with open(bot_info,"w+",encoding="utf-8") as g:
         g.write(str(user_id))
         g.close()
+    
+    superusers = bot.config.superusers
 
-    for superuser in bot.config.superusers:
-        await bot.send_private_msg(user_id=int(superuser), message=Message("Bot启动成功"))
+    gl = await bot.get_friend_list()
+    friends = ["{user_id}".format_map(g) for g in gl]
+    for superuser in superusers:
+        if not superuser in friends:
+            raise KeyboardInterrupt("存在非bot好友的超级用户, 请先添加Bot为好友!")
+
+    for superuser in superusers:
+        await bot.send_private_msg(
+            user_id=int(superuser),
+            message=Message("Bot启动成功")
+        )
