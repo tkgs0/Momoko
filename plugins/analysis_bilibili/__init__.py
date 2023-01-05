5c1387a836cb848ad97893c08a673bcb9bb94738
tkgs0 <hgc.sakuya@ya.ru>
2023-01-05 18:54:45 +0800

    :twisted_rightwards_arrows:

--- plugins/analysis_bilibili/__init__.py
+++ plugins/analysis_bilibili/__init__.py
@@ -1,11 +1,8 @@
 import re
-
 from nonebot import on_regex
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageEvent
 from nonebot.log import logger
-
-from .analysis_bilibili import b23_extract, bili_keyword
-from .config import config
+from .analysis_bilibili import config, b23_extract, bili_keyword
 
 analysis_bili = on_regex(
     r"(b23.tv)|(bili(22|23|33|2233).cn)|(.bilibili.com)|(^(av|cv)(\d+))|(^BV([a-zA-Z0-9]{10})+)|"
@@ -13,21 +10,24 @@ analysis_bili = on_regex(
     flags=re.I,
 )
 
+blacklist = getattr(config, "analysis_blacklist", [])
+group_blacklist = getattr(config, "analysis_group_blacklist", [])
+
 
 @analysis_bili.handle()
 async def analysis_main(event: MessageEvent) -> None:
     text = str(event.message).strip()
-    if event.sender.user_id in config.analysis_blacklist:
+    if blacklist and int(event.get_user_id()) in blacklist:
         return
-    if isinstance(event, GroupMessageEvent):
-        group_id = event.group_id
-        if group_id in config.analysis_group_blacklist:
-            return
-    else:
-        group_id = None
     if re.search(r"(b23.tv)|(bili(22|23|33|2233).cn)", text, re.I):
         # 提前处理短链接，避免解析到其他的
         text = await b23_extract(text)
+    if isinstance(event, GroupMessageEvent):
+        group_id = event.group_id
+    else:
+        group_id = None
+    if group_id in group_blacklist:
+        return
     msg = await bili_keyword(group_id, text)
     if msg:
         try:
