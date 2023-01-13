dfefb860753b3b1ddc456258a75612c8870cd1c8
tkgs0 <hgc.sakuya@ya.ru>
2023-01-13 21:58:44 +0800

    :art:

--- plugins/smart_reply/__init__.py
+++ plugins/smart_reply/__init__.py
@@ -6,8 +6,13 @@ from nonebot.adapters.onebot.v11 import (
     PokeNotifyEvent,
     MessageSegment
 )
-import asyncio
-from .utils import *
+import asyncio, random, re
+from .utils import (
+    # Bot_NICKNAME,
+    hello__reply,
+    get_chat_result,
+    get_reply
+)
 
 
 
@@ -17,8 +22,8 @@ poke_ = on_notice(priority=99, block=False)
 async def _(event: PokeNotifyEvent):
     if event.self_id == event.target_id:
         await asyncio.sleep(random.random()+1)
-        # await poke_.finish(f"请不要戳{Bot_NICKNAME}>_<")
-        await poke_.finish(MessageSegment("poke", {"qq": event.user_id}))
+        # await poke_.finish(f'请不要戳{Bot_NICKNAME}>_<')
+        await poke_.finish(MessageSegment('poke', {'qq': event.user_id}))
 
 
 
@@ -30,27 +35,26 @@ async def _(event: MessageEvent):
     # 获取消息文本
     msg = str(event.get_message())
     # 去掉带中括号的内容(去除cq码)
-    msg = re.sub(r"\[.*?\]", "", msg)
+    msg = re.sub(r'\[[^\[\]]*\]', '', msg)
 
     await asyncio.sleep(random.random()*2+1)
 
     # 如果是光艾特bot(没消息返回)或者打招呼的话,就回复以下内容
     if (not msg) or msg.isspace() or msg in [
-        "你好啊",
-        "你好",
-        "在吗",
-        "在不在",
-        "您好",
-        "您好啊",
-        "你好",
-        "在",
+        '你好啊',
+        '你好',
+        '在吗',
+        '在不在',
+        '您好',
+        '您好啊',
+        '你好',
+        '在',
     ]:
         await ai.finish(Message(random.choice(hello__reply)))
     # 从字典里获取结果
     result = await get_chat_result(msg)
     # 如果词库没有结果，则调用ownthink获取智能回复
     if result == None:
-        url = f"https://api.ownthink.com/bot?appid=xiaosi&userid=user&spoken={msg}"
-        content = await get_reply(url)
+        content = await get_reply(msg)
         await ai.finish(content)
     await ai.finish(Message(result))
--- plugins/smart_reply/utils.py
+++ plugins/smart_reply/utils.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 import random
 import nonebot
 try:
@@ -7,21 +6,21 @@ try:
 except ModuleNotFoundError:
     import json
 from httpx import AsyncClient
-import re
+
 
 Bot_NICKNAME: str = list(nonebot.get_driver().config.nickname)[0]      # bot的nickname,可以换成你自己的
 Bot_MASTER: str = list(nonebot.get_driver().config.superusers)[0]      # bot的主人名称,也可以换成你自己的
 
 # 载入词库(这个词库有点涩)
-AnimeThesaurus = json.load(open(Path(os.path.join(os.path.dirname(
-    __file__), "resource")) / "data.json", "r", encoding="utf8"))
+AnimeThesaurus = json.load(open(Path(__file__).parent / 'resource' / 'data.json', 'r', encoding='utf8'))
 
 # hello之类的回复
 hello__reply = [
     'ʕ  •ᴥ•ʔ ?',
 ]
 
-# 从字典里返还消息, 抄(借鉴)的zhenxun-bot
+
+# 从字典里返还消息, 借鉴(抄)的zhenxun-bot
 async def get_chat_result(text: str):
     if len(text) < 7:
         keys = AnimeThesaurus.keys()
@@ -29,16 +28,31 @@ async def get_chat_result(text: str):
             if text.find(key) != -1:
                 return random.choice(AnimeThesaurus[key])
 
+
+headers = {
+    'referer': 'https://www.ownthink.com/',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+}
+
+
 # 从ownthink_api拿到消息
-async def get_reply(url):
+async def get_reply(msg):
+
+    url = f'https://api.ownthink.com/bot'
+    params = {
+        'appid': 'xiaosi',
+        'userid': 'user',
+        'spoken': msg,
+    }
+
     async with AsyncClient() as client:
-        response = await client.get(url)
-        if response.json()["data"]["type"] == 5000:
-            res = response.json()["data"]["info"]["text"].replace("小思", Bot_NICKNAME)
-            res = re.sub(u"\\{.*?\\}", "", res)
-            return res
-        else:
-            return 'ʕ  •ᴥ•ʔ……'
-        
-        
+        try:
+            response = await client.get(url=url, params=params, headers=headers)
+            if response.json()['data']['type'] == 5000:
+                res = response.json()['data']['info']['text'].replace('小思', Bot_NICKNAME)
+                return res
+            else:
+                return 'ʕ  •ᴥ•ʔ……'
+        except Exception as e:
+            return repr(e)
 
