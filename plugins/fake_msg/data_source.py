8619a33dd4af294afe32a5fa77dfc48f2cd66cbe
tkgs0 <hgc.sakuya@ya.ru>
2023-01-17 23:29:20 +0800

    :art:

--- plugins/fake_msg/data_source.py
+++ plugins/fake_msg/data_source.py
@@ -4,14 +4,32 @@ class Funny():
 
     @staticmethod
     def fake_msg(text: str) -> list:
-        arg = text.split("\n")
+        arg = text.strip().split("\n")
         node = list()
     
         for i in arg:
             args = i.split("-")
-            qq = args[0]
+            if is_number(args[0]):
+                qq = args[0]
+            else:
+                raise TypeError('qq号须为数字!')
             name = unescape(args[1])
             repo = unescape(args[2].replace("\\n","\n"))
             dic = {"type": "node", "data": {"name": name, "uin": qq, "content": repo}}
             node.append(dic)
         return node
+
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
