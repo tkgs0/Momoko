5baae3dda7383326384795cb8413a05b0e6e4389
tkgs0 <hgc.sakuya@ya.ru>
2023-01-29 07:48:25 +0000

    :construction:

---
+++ bot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nonebot
+from nonebot.adapters.onebot.v12 import Adapter as ONEBOT_V12Adapter
+
+
+nonebot.init()
+app = nonebot.get_asgi()
+driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V12Adapter)
+nonebot.load_from_toml("pyproject.toml")
+
+
+if __name__ == "__main__":
+    nonebot.run(app="__mp_main__:app")
