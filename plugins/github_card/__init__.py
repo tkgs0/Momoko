a68b0a6e42eaa830ea43e712d8e75bf84f75878d
tkgs0 <hgc.sakuya@ya.ru>
2023-02-03 05:33:53 +0000

    :art:

--- plugins/github_card/__init__.py
+++ plugins/github_card/__init__.py
@@ -1,13 +1,15 @@
-from nonebot.rule import T_State
 from nonebot import on_message, get_driver
 from nonebot.adapters.onebot.v11 import (
     GroupMessageEvent,
-    Message,
+    MessageSegment,
     unescape
 )
 import re
 from .config import Config
-from .data_source import get_github_reposity_information
+from .data_source import (
+    get_github_reposity_information,
+    get_url,
+)
 
 
 __plugin_name__ = "Github仓库信息卡片"
@@ -19,10 +21,10 @@ github = on_message(priority=5, block=False)
 
 
 @github.handle()
-async def github_handle(event: GroupMessageEvent, state: T_State):
+async def github_handle(event: GroupMessageEvent):
     url = unescape(event.get_plaintext())
-    if re.match("https://github.com/.*?/.*?", url) != None:
+    if re.match("https://github.com/.*?/.*?", url):
         imageUrl = await get_github_reposity_information(url)
         assert(imageUrl != "获取信息失败")
-        await github.send(Message(f"[CQ:image,file={imageUrl}]"))
-        await github.finish(Message(f"[CQ:image,file=https://image.thum.io/get/width/1280/crop/1440/viewportWidth/1280/png/noanimate/{url}]"))
+        await github.send(MessageSegment.image(await get_url(imageUrl)))
+        await github.finish(MessageSegment.image(await get_url(f"https://image.thum.io/get/width/1280/crop/1440/viewportWidth/1280/png/noanimate/{url}")))
--- plugins/github_card/data_source.py
+++ plugins/github_card/data_source.py
@@ -1,17 +1,28 @@
-import aiohttp
-import asyncio
+from httpx import AsyncClient
 
-Headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 YaBrowser/23.0.0.00.00 SA/3 Safari/537.36"}
+
+Headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 YaBrowser/23.0.0.00.00 SA/3 Safari/537.36"
+}
 
 
 async def get_github_reposity_information(url: str) -> str:
     try:
         UserName, RepoName = url.replace("https://github.com/", "").split("/")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.github.com/users/{UserName}", headers=Headers, timeout=5) as response:
-                RawData = await response.json()
-                AvatarUrl = RawData["avatar_url"]
-                ImageUrl = f"https://image.thum.io/get/width/1280/crop/640/viewportWidth/1280/png/noanimate/https://socialify.git.ci/{UserName}/{RepoName}/image?description=1&font=Rokkitt&forks=1&issues=1&language=1&owner=1&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Dark&logo={AvatarUrl}"
-                return ImageUrl
+        async with AsyncClient() as client:
+            res = await client.get(f"https://api.github.com/users/{UserName}", headers=Headers, timeout=5)
+            RawData = res.json()
+            AvatarUrl = RawData["avatar_url"]
+            ImageUrl = f"https://image.thum.io/get/width/1280/crop/640/viewportWidth/1280/png/noanimate/https://socialify.git.ci/{UserName}/{RepoName}/image?description=1&font=Rokkitt&forks=1&issues=1&language=1&owner=1&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Dark&logo={AvatarUrl}"
+            await res.aclose()
+            return ImageUrl
     except:
         return "获取信息失败"
+
+
+async def get_url(url: str) -> str | bytes:
+    async with AsyncClient() as client:
+        res = await client.get(url, headers=Headers, timeout=30)
+        img = res.content
+        await res.aclose()
+        return img
