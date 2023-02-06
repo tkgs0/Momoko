29025342b5fad847d2bba46cb55460c9b82823bb
tkgs0 <hgc.sakuya@ya.ru>
2023-02-06 05:46:55 +0000

    :sparkles:异步调用命令行

--- basic_plugins/sys_cmd_/__init__.py
+++ basic_plugins/sys_cmd_/__init__.py
@@ -1,4 +1,7 @@
+from platform import system
 from subprocess import Popen, PIPE
+from asyncio import create_subprocess_shell
+from asyncio.subprocess import PIPE as AsyncPIPE
 from random import choice
 from nonebot import on_command
 from nonebot.permission import SUPERUSER
@@ -15,35 +18,96 @@ from nonebot.adapters.onebot.v11.helpers import Cooldown
 
 
 
-def help() -> str:
-    return (
-        "调用系统命令行\n"
-        "⚠危险操作, 谨慎使用!\n\n"
-        ">cmd {命令}\n"
-        "For example:\n"
-        ">cmd echo \"Hello World\""
-    )
+cmd_help = (
+    '调用系统命令行\n'
+    '⚠危险操作, 谨慎使用!\n\n'
+    '>cmd {命令}\n'
+    'For example:\n'
+    '>cmd echo "Hello World"'
+)
 
 
-_flmt_notice = choice(["慢...慢一..点❤", "冷静1下", "歇会歇会~~"])
+shell_help = (
+    '调用系统命令行\n'
+    '(不支持Windows)\n'
+    '⚠危险操作, 谨慎使用!\n\n'
+    '>shell {命令}\n'
+    'For example:\n'
+    '>shell echo "Hello World"'
+)
 
 
-sys_cmd = on_command('>cmd', priority=1, block=True, permission=SUPERUSER)
+_win = ('windows', 'Windows', 'win32', 'Win32', 'win16', 'Win16')
+
+
+_flmt_notice = choice(['慢...慢一..点❤', '冷静1下', '歇会歇会~~'])
+
+
+sys_shell = on_command(
+    '>shell',
+    permission=SUPERUSER,
+    priority=1,
+    block=True
+)
+
+@sys_shell.handle([Cooldown(5, prompt=_flmt_notice)])
+async def _(matcher: Matcher, args: Message = CommandArg()):
+    for i in _win:
+        if system() and (system() in i or i in system()):
+            await sys_shell.finish('暂不支持Windows,\n请使用同步方法 `>cmd`')
+    msg = args.extract_plain_text()
+    if msg:
+        matcher.set_arg('opt', args)
+
+
+@sys_shell.got('opt', prompt='请输入命令内容\n获取帮助：>shell.help')
+async def _(opt: str = ArgPlainText('opt')):
+
+    # 拯救傻瓜用户
+    if opt.startswith('>shell.help'):
+        await sys_shell.finish(shell_help)
+
+    content = await (await create_subprocess_shell(
+        unescape(opt),
+        stdin=AsyncPIPE,
+        stdout=AsyncPIPE,
+        stderr=AsyncPIPE
+    )).communicate()
+    
+    if content == (b'', b''):
+        msg = '\n执行完毕, 没有任何输出呢~'
+    elif content[1] == b'':
+        msg = f'\nstdout:\n{content[0].decode()}\n>执行完毕'
+    elif content[0] == b'':
+        msg = f'\nstderr:\n{content[1].decode()}'
+    else :
+        msg = (f'\nstdout:\n{content[0].decode()}'
+               f'\nstderr:\n{content[1].decode()}'
+               '\n>执行完毕')
+    await sys_shell.finish(msg, at_sender=True)
+
+
+sys_cmd = on_command(
+    '>cmd',
+    priority=1,
+    block=True,
+    permission=SUPERUSER
+)
 
 
 @sys_cmd.handle([Cooldown(5, prompt=_flmt_notice)])
 async def _(matcher: Matcher, args: Message = CommandArg()):
     msg = args.extract_plain_text()
     if msg:
-        matcher.set_arg("opt", args)
+        matcher.set_arg('opt', args)
 
 
-@sys_cmd.got("opt", prompt="请输入命令内容\n获取帮助：>cmd.help")
-async def _(opt: str = ArgPlainText("opt")):
+@sys_cmd.got('opt', prompt='请输入命令内容\n获取帮助：>cmd.help')
+async def _(opt: str = ArgPlainText('opt')):
 
     # 拯救傻瓜用户
-    if opt.startswith(">cmd.help"):
-        await sys_cmd.finish(help())
+    if opt.startswith('>cmd.help'):
+        await sys_cmd.finish(cmd_help)
 
     content = Popen(
         unescape(opt),
@@ -54,15 +118,14 @@ async def _(opt: str = ArgPlainText("opt")):
         universal_newlines=True
     ).communicate()
 
-    msg = None
     if content == ('',''):
-        msg = "\n执行完毕, 没有任何输出呢~"
+        msg = '\n执行完毕, 没有任何输出呢~'
     elif content[1] == '':
-        msg = f"\nstdout:\n{content[0]}\n>执行完毕"
+        msg = f'\nstdout:\n{content[0]}\n>执行完毕'
     elif content[0] == '':
-        msg = f"\nstderr:\n{content[1]}"
+        msg = f'\nstderr:\n{content[1]}'
     else :
-        msg = f"\nstdout:\n{content[0]}\nstderr:\n{content[1]}\n>执行完毕"
+        msg = f'\nstdout:\n{content[0]}\nstderr:\n{content[1]}\n>执行完毕'
     await sys_cmd.finish(msg, at_sender=True)
 
 
@@ -70,13 +133,13 @@ sys_cmd_helper = on_command('>cmd.help', priority=5, block=True)
 
 @sys_cmd_helper.handle()
 async def _():
-    await sys_cmd_helper.finish(help())
+    await sys_cmd_helper.finish(cmd_help)
 
 
 
 
 upload_group_file = on_command(
-    ">上传文件",
+    '>上传文件',
     permission=SUPERUSER,
     priority=1,
     block=True,
@@ -86,7 +149,7 @@ upload_group_file = on_command(
 async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     args = arg.extract_plain_text().strip().split(maxsplit=1)
     if len(args) < 2:
-        await upload_group_file.finish("用法:\n>上传文件 文件名 本地路径")
+        await upload_group_file.finish('用法:\n>上传文件 文件名 本地路径')
     filename = unescape(args[0])
     filepath = unescape(args[1])
     try:
@@ -101,7 +164,7 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
 
 
 upload_private_file = on_command(
-    ">私发文件",
+    '>私发文件',
     permission=SUPERUSER,
     priority=1,
     block=True,
@@ -111,7 +174,7 @@ upload_private_file = on_command(
 async def _(bot: Bot, arg: Message = CommandArg()):
     args = arg.extract_plain_text().strip().split(maxsplit=2)
     if len(args) < 3 or not is_number(args[0]):
-        await upload_private_file.finish("用法:\n>私发文件 目标QQ 文件名 本地路径")
+        await upload_private_file.finish('用法:\n>私发文件 目标QQ 文件名 本地路径')
     uid = args[0]
     filename = unescape(args[1])
     filepath = unescape(args[2])
