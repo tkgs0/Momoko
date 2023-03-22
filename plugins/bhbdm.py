9a94a5a9e99b2880ae1c33dc5261a5f2828ba15e
tkgs0 <hgc.sakuya@ya.ru>
2023-03-22 15:30:36 +0000

    :poop:

--- plugins/bhbdm.py
+++ plugins/bhbdm.py
@@ -2,41 +2,36 @@ from nonebot.adapters import Message
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
 from urllib.parse import quote
+import base64 as b64
 
 
 
-def run(msg: str, x: str) -> str:
-    _engine = {
-        'google': 'https://www.google.com/search?lr=lang_zh-CN%7Clang_zh-TW&q=',
-        'yandex': 'https://yandex.com/search/?text=',
-        'baidu': 'https://buhuibaidu.me/?s='
-    }
-    msg = _engine[x] + quote(msg)
-    return msg
-
+url = [
+    'https://www.google.com/search?lr=lang_zh-CN%7Clang_zh-TW&q=',
+    'https://yandex.com/search/?text=',
+    'https://baidu.ma/?q='
+]
+    
 
 
 to_gg = on_command("/谷歌搜索", priority=5, block=True)
 
 @to_gg.handle()
-async def _(message: Message = CommandArg()):
-    url = run(str(message), 'google')
-    await to_gg.finish(url)
+async def _(arg: Message = CommandArg()):
+    await to_gg.finish(url[0] + quote(arg.extract_plain_text()))
 
 
 
 to_ydx = on_command("/毛哥搜索", priority=5, block=True)
 
 @to_ydx.handle()
-async def _(message: Message = CommandArg()):
-    url = run(str(message), 'yandex')
-    await to_ydx.finish(url)
+async def _(arg: Message = CommandArg()):
+    await to_ydx.finish(url[1] + quote(arg.extract_plain_text()))
 
 
 
 bhbdm = on_command("/百度搜索", priority=5, block=True)
 
 @bhbdm.handle()
-async def _(message: Message = CommandArg()):
-    url = run(str(message), 'baidu')
-    await bhbdm.finish(url)
+async def _(arg: Message = CommandArg()):
+    await bhbdm.finish(url[2] + b64.b64encode(arg.extract_plain_text().encode()).decode())
