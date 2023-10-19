a663a6329272e6fbe104604bb7c2e2ed344d5ac4
tkgs0 <hgc.sakuya@ya.ru>
2023-10-19 01:34:45 +0000

    :bug:

--- plugins/code_runner/__init__.py
+++ plugins/code_runner/__init__.py
@@ -1,7 +1,7 @@
 from random import choice
 from nonebot import on_command, get_driver
+from nonebot.params import CommandArg
 from nonebot.matcher import Matcher
-from nonebot.params import CommandArg, ArgPlainText
 from nonebot.adapters.onebot.v11 import Message, unescape
 from nonebot.adapters.onebot.v11.helpers import Cooldown
 
@@ -19,35 +19,20 @@ code_runner = on_command('>code', priority=6, block=True)
 @code_runner.handle([Cooldown(5, prompt=_flmt_notice)])
 async def _(matcher: Matcher, args: Message = CommandArg()):
     msg = args.extract_plain_text()
-
     if msg:
-        matcher.set_arg("opt", args)
-    else:
-        await code_runner.finish("请发送 >code.help 以获取帮助~！")
+        matcher.state["opt"] = args
 
 
 @code_runner.got("opt", prompt="需要运行的语言及代码？\n获取帮助：>code.help")
-async def _(opt: str = ArgPlainText("opt")):
+async def _(matcher: Matcher):
+    opt = matcher.state["opt"].extract_plain_text()
 
-    # 拯救傻瓜用户
-    if opt == ">code.help":
+                        # 拯救傻瓜用户
+    if opt in ('.help', '>code.help'):
         await code_runner.finish(CodeRunner().help())
+
+    if opt == '.list':
+        await code_runner.finish(CodeRunner().list_supp_lang())
 
     content = str(await CodeRunner().runner(glot_token, unescape(opt)))
     await code_runner.finish(content, at_sender=True)
-
-
-code_runner_helper = on_command('>code.help', priority=5, block=True)
-
-
-@code_runner_helper.handle()
-async def _():
-    await code_runner_helper.finish(CodeRunner().help())
-
-
-code_supp_list = on_command('>code.list', priority=5, block=True)
-
-
-@code_supp_list.handle()
-async def _():
-    await code_supp_list.finish(CodeRunner().list_supp_lang())
