791d76011fc757268d93f9b3dafea8673d3f514e
tkgs0 <hgc.sakuya@ya.ru>
2023-10-20 05:44:35 +0000

    :bug:

--- basic_plugins/manager/bot_id.py
+++ basic_plugins/manager/bot_id.py
@@ -4,16 +4,7 @@ from nonebot.adapters.onebot.v11 import (
     Message,
     LifecycleMetaEvent
 )
-try:
-    import ujson as json
-except ModuleNotFoundError:
-    import json
-from pathlib import Path
 
-
-
-bot_info = Path() / "data" / "bot_id.json"
-bot_info.parent.mkdir(parents=True, exist_ok=True)
 
 async def check_first_connect(_: LifecycleMetaEvent) -> bool:
     return True
@@ -21,8 +12,7 @@ async def check_first_connect(_: LifecycleMetaEvent) -> bool:
 start_metaevent = on_metaevent(rule=check_first_connect, temp=True)
 @start_metaevent.handle()
 async def start(bot: Bot) -> None:
-    user_id = await bot.get_login_info()
-    bot_info.write_text(json.dumps(user_id, ensure_ascii=False), 'utf-8')
+    bot_info = await bot.get_login_info()
     
     superusers = bot.config.superusers
 
@@ -30,7 +20,7 @@ async def start(bot: Bot) -> None:
     friends = ["{user_id}".format_map(g) for g in gl]
     for superuser in superusers:
         if not superuser in friends:
-            raise KeyboardInterrupt("\033[40;33;1m存在非bot好友的超级用户, 请先添加Bot为好友!\033[0m")
+            raise KeyboardInterrupt(f"\n\033[40;33;1mBot: {bot_info['user_id']}\n存在非bot好友的超级用户 {superuser}, 请先添加Bot为好友!\033[0m")
 
     for superuser in superusers:
         await bot.send_private_msg(
