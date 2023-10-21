814f7031fd47e8ad3c36b91c71886020cf124145
tkgs0 <hgc.sakuya@ya.ru>
2023-10-21 06:50:08 +0000

    commit

--- plugins/anti_recall/__init__.py
+++ plugins/anti_recall/__init__.py
@@ -1,7 +1,11 @@
-from nonebot import on_notice
+import ujson as json
+from pathlib import Path
+from nonebot import on_notice, on_command
 from nonebot.log import logger
+from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import (
     Bot, 
+    MessageEvent,
     GroupRecallNoticeEvent, 
     FriendRecallNoticeEvent, 
     Message,
@@ -10,12 +14,80 @@ from nonebot.adapters.onebot.v11 import (
 from .utils import recall_msg_dealer, MessageChecker
 
 
+switch_file = Path() / 'data' / 'anti_recall' / 'anti_recall.json'
+switch_file.parent.mkdir(parents=True, exist_ok=True)
+
+switch = (
+    json.loads(switch_file.read_text('utf-8'))
+    if switch_file.is_file()
+    else {}
+)
+
+
+def save_switch() -> None:
+    switch_file.write_text(
+        json.dumps(
+            switch,
+            ensure_ascii=False,
+            escape_forward_slashes=False,
+            indent=2
+        ),
+        encoding='utf-8'
+    )
+
+
+def check_self_id(self_id) -> str:
+    self_id = f'{self_id}'
+
+    if not switch.get(self_id):
+        switch.update({
+            self_id: False
+        })
+        save_switch()
+
+    return self_id
+
+
+enable_anti_recall = on_command(
+    '开启防撤回',
+    aliases={'开启反撤回', '启用防撤回', '启用反撤回'},
+    permission=SUPERUSER,
+    priority=2,
+    block=True
+)
+
+@enable_anti_recall.handle()
+async def _(event: MessageEvent):
+    self_id = check_self_id(event.self_id)
+    switch[self_id] = True
+    save_switch()
+    await enable_anti_recall.finish('反撤回已开启.')
+
+
+disable_anti_recall = on_command(
+    '关闭防撤回',
+    aliases={'关闭反撤回', '禁用防撤回', '禁用反撤回'},
+    permission=SUPERUSER,
+    priority=2,
+    block=True
+)
+
+@disable_anti_recall.handle()
+async def _(event: MessageEvent):
+    self_id = check_self_id(event.self_id)
+    switch[self_id] = False
+    save_switch()
+    await disable_anti_recall.finish('反撤回已关闭.')
+
 
 recall_event = on_notice(priority=1, block=False)
 
-
 @recall_event.handle()
 async def _(bot: Bot, event: FriendRecallNoticeEvent):
+    self_id = check_self_id(event.self_id)
+    if not switch[self_id]:
+        return
+
     if event.is_tome():
         return
 
@@ -43,6 +115,10 @@ async def _(bot: Bot, event: FriendRecallNoticeEvent):
 
 @recall_event.handle()
 async def _(bot: Bot, event: GroupRecallNoticeEvent):
+    self_id = check_self_id(event.self_id)
+    if not switch[self_id]:
+        return
+
     if event.is_tome():
         return
 
