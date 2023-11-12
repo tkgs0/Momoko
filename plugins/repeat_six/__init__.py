80f4843fb7c74959458f3572d876269398d503a5
tkgs0 <hgc.sakuya@ya.ru>
2023-11-12 09:37:54 +0000

    commit

---
+++ plugins/repeat_six/__init__.py
@@ -0,0 +1,23 @@
+from random import random
+from nonebot import on_regex
+from nonebot.internal.adapter import Event
+
+
+six = on_regex(r"^6+$", priority=5, block=False)
+
+@six.handle()
+async def _(event: Event):
+    if not random()*10//1%6:
+        await six.finish(event.get_message())
+
+
+nonsense = on_regex(
+    r"^(蚌|蜯|草|艹|乐|樂|寄|典|孝|急|麻)$",
+    priority=5,
+    block=False
+)
+
+@nonsense.handle()
+async def _(event: Event):
+    if not random()*10//1%6:
+        await nonsense.finish(event.get_message())
