f6505daeae9beaf66399d65171309d5015306e5c
tkgs0 <hgc.sakuya@ya.ru>
2023-11-29 11:33:45 +0000

    :sparkles:

---
+++ other_plugins/cocdicer/__init__.py
@@ -0,0 +1,143 @@
+from .util import MessageEvent, GroupMessageEvent
+from .dices import help_message, st, en, rd0
+from .madness import ti, li
+from .investigator import Investigator
+from .san_check import sc
+from .cards import _cachepath, cards, cache_cards, set_handler, show_handler, sa_handler, del_handler
+
+from nonebot import get_driver, get_bot
+from nonebot.rule import Rule
+from nonebot.matcher import Matcher
+from nonebot.plugin import on_startswith
+from nonebot.adapters import Bot as Bot
+from nonebot.adapters.onebot.v11 import Bot as V11Bot
+from nonebot.adapters.onebot.v12 import Bot as V12Bot
+
+
+import os
+
+driver = get_driver()
+
+
+@driver.on_startup
+async def _():  # 角色卡暂存目录初始化
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    if not os.path.exists(_cachepath):
+        with open(_cachepath, "w", encoding="utf-8") as f:
+            f.write("{}")
+    cards.load()
+
+
+def is_group_message() -> Rule:
+    async def _is_group_message(bot: Bot, event: MessageEvent) -> bool:
+        return True if type(event) is GroupMessageEvent else False
+    return Rule(_is_group_message)
+
+
+rdhelp = on_startswith(".h", priority=2, block=True)
+stcommand = on_startswith(".st", priority=2, block=True)
+encommand = on_startswith(".en", priority=2, block=True)
+ticommand = on_startswith(".ti", priority=2, block=True)
+licommand = on_startswith(".li", priority=2, block=True)
+coccommand = on_startswith(".coc", priority=2, block=True)
+sccommand = on_startswith(".sc", priority=2, block=True)
+rhcommand = on_startswith(".rh", priority=3, block=True)
+rdcommand = on_startswith(".r", priority=4, block=True)
+setcommand = on_startswith(".set", priority=5, block=True)
+showcommand = on_startswith(".show", priority=5, block=True)
+sacommand = on_startswith(".sa", priority=5, block=True)
+delcommand = on_startswith(".del", priority=5, block=True)
+
+
+@rdhelp.handle()
+async def rdhelphandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[5:].strip()
+    await matcher.finish(help_message(args))
+
+
+@stcommand.handle()
+async def stcommandhandler(matcher: Matcher):
+    await matcher.finish(st())
+
+
+@encommand.handle()
+async def enhandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[3:].strip()
+    await matcher.finish(en(args))
+
+
+@rhcommand.handle()
+async def rhcommandhandler(bot: Bot, event: GroupMessageEvent):
+    args = str(event.get_message())[3:].strip()
+    uid = event.get_user_id()
+    if args and not("." in args):
+        print("get here")
+        if isinstance(bot, V12Bot):
+            from nonebot.adapters.onebot.v12 import  MessageSegment
+            await bot.send_message(detail_type="private", user_id=uid, message=[MessageSegment.text(rd0(args))])
+        elif isinstance(bot, V11Bot):
+            await bot.send_private_msg(user_id=uid, message=rd0(args))
+
+
+@rdcommand.handle()
+async def rdcommandhandler(event: MessageEvent):
+    args = str(event.get_message())[2:].strip()
+    if args and not("." in args):
+        await rdcommand.finish(rd0(args))
+
+
+@coccommand.handle()
+async def cochandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[4:].strip()
+    try:
+        args = int(args)
+    except ValueError:
+        args = 20
+    inv = Investigator()
+    await matcher.send(inv.age_change(args))
+    if 15 <= args < 90:
+        cache_cards.update(event, inv.__dict__, save=False)
+        await matcher.finish(inv.output())
+
+
+@ticommand.handle()
+async def ticommandhandler(matcher: Matcher,):
+    await matcher.finish(ti())
+
+
+@licommand.handle()
+async def licommandhandler(matcher: Matcher,):
+    await matcher.finish(li())
+
+
+@sccommand.handle()
+async def schandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[3:].strip().lower()
+    await matcher.finish(sc(args, event=event))
+
+
+@setcommand.handle()
+async def sethandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[4:].strip().lower()
+    await matcher.finish(set_handler(event, args))
+
+
+@showcommand.handle()
+async def showhandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[5:].strip().lower()
+    for msg in show_handler(event, args):
+        await matcher.send(msg)
+
+
+@sacommand.handle()
+async def sahandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[3:].strip().lower()
+    await matcher.finish(sa_handler(event, args))
+
+
+@delcommand.handle()
+async def delhandler(matcher: Matcher, event: MessageEvent):
+    args = str(event.get_message())[4:].strip().lower()
+    for msg in del_handler(event, args):
+        await matcher.send(msg)
---
+++ other_plugins/cocdicer/cards.py
@@ -0,0 +1,189 @@
+from typing import Dict, List, Optional
+from .investigator import Investigator
+from .util import MessageEvent, GroupMessageEvent
+from .messages import help_messages
+from .dices import expr
+
+import diro
+
+try:
+    import ujson as json
+except ModuleNotFoundError:
+    import json
+
+import os
+import re
+_cachepath = os.path.join("data", "coc_cards.json")
+
+
+def get_group_id(event: MessageEvent):
+    if type(event) is GroupMessageEvent:
+        return str(event.group_id)
+    else:
+        return "0"
+
+
+class Cards():
+    def __init__(self) -> None:
+        self.data: Dict[str, dict] = {}
+
+    def save(self) -> None:
+        with open(_cachepath, "w", encoding="utf-8") as f:
+            json.dump(self.data, f, ensure_ascii=False)
+
+    def load(self) -> None:
+        with open(_cachepath, "r", encoding="utf-8") as f:
+            self.data = json.load(f)
+
+    def update(self, event: MessageEvent, inv_dict: dict, qid: str = "", save: bool = True):
+        group_id = get_group_id(event)
+        if not self.data.get(group_id):
+            self.data[group_id] = {}
+        self.data[group_id].update(
+            {qid if qid else str(event.sender.user_id): inv_dict})
+        if save:
+            self.save()
+
+    def get(self, event: MessageEvent, qid: str = "") -> Optional[Dict[str, dict]]:
+        group_id = get_group_id(event)
+        if self.data.get(group_id):
+            if self.data[group_id].get(qid if qid else str(event.sender.user_id)):
+                return self.data[group_id].get(qid if qid else str(event.sender.user_id))
+        else:
+            return None
+
+    def delete(self, event: MessageEvent, qid: str = "", save: bool = True) -> bool:
+        if self.get(event, qid=qid):
+            if self.data[get_group_id(event)].get(qid if qid else str(event.sender.user_id)):
+                self.data[get_group_id(event)].pop(
+                    qid if qid else str(event.sender.user_id))
+            if save:
+                self.save()
+            return True
+        return False
+
+    def delete_skill(self, event: MessageEvent, skill_name: str, qid: str = "", save: bool = True) -> bool:
+        if self.get(event, qid=qid):
+            data = self.get(event, qid=qid)
+            if data["skills"].get(skill_name):
+                data["skills"].pop(skill_name)
+                self.update(event, data, qid=qid, save=save)
+                return True
+        return False
+
+
+cards = Cards()
+cache_cards = Cards()
+attrs_dict: Dict[str, List[str]] = {
+    "名字": ["name", "名字", "名称"],
+    "年龄": ["age", "年龄"],
+    "力量": ["str", "力量"],
+    "体质": ["con", "体质"],
+    "体型": ["siz", "体型"],
+    "敏捷": ["dex", "敏捷"],
+    "外貌": ["app", "外貌"],
+    "智力": ["int", "智力", "灵感"],
+    "意志": ["pow", "意志"],
+    "教育": ["edu", "教育"],
+    "幸运": ["luc", "幸运"],
+    "理智": ["san", "理智"],
+}
+
+
+def set_handler(event: MessageEvent, args: str):
+    if not args:
+        if cache_cards.get(event):
+            card_data = cache_cards.get(event)
+            cards.update(event, inv_dict=card_data)
+            inv = Investigator().load(card_data)
+            return "成功从缓存保存人物卡属性：\n" + inv.output()
+        else:
+            return "未找到缓存数据，请先使用coc指令生成角色"
+    else:
+        args = args.split(" ")
+        if cards.get(event):
+            card_data = cards.get(event)
+            inv = Investigator().load(card_data)
+        else:
+            return "未找到已保存数据，请先使用空白set指令保存角色数据"
+        if len(args) >= 2:
+            for attr, alias in attrs_dict.items():
+                if args[0] in alias:
+                    if attr == "名字":
+                        inv.__dict__[alias[0]] = args[1]
+                    else:
+                        try:
+                            inv.__dict__[alias[0]] = int(args[1])
+                        except ValueError:
+                            return "请输入正整数属性数据"
+                    cards.update(event, inv.__dict__)
+                    return "设置调查员%s为：%s" % (attr, args[1])
+            try:
+                inv.skills[args[0]] = int(args[1])
+                cards.update(event, inv.__dict__)
+                return "设置调查员%s技能为：%s" % (args[0], args[1])
+            except ValueError:
+                return "请输入正整数技能数据"
+
+
+def show_handler(event: MessageEvent, args: str):
+    r = []
+    if not args:
+        if cards.get(event):
+            card_data = cards.get(event)
+            inv = Investigator().load(card_data)
+            r.append("使用中人物卡：\n" + inv.output())
+        if cache_cards.get(event):
+            card_data = cache_cards.get(event)
+            inv = Investigator().load(card_data)
+            r.append("已暂存人物卡：\n" + inv.output())
+    elif args == "s":
+        if cards.get(event):
+            card_data = cards.get(event)
+            inv = Investigator().load(card_data)
+            r.append(inv.skills_output())
+    elif re.search(r"\[cq:at,qq=\d+\]", args):
+        qid = re.search(r"\[cq:at,qq=\d+\]", args).group()[10:-1]
+        if cards.get(event, qid=qid):
+            card_data = cards.get(event, qid=qid)
+            inv = Investigator().load(card_data)
+            r.append("查询到人物卡：\n" + inv.output())
+            if args[0] == "s":
+                r.append(inv.skills_output())
+    if not r:
+        r.append("无保存/暂存信息")
+    return r
+
+
+def del_handler(event: MessageEvent, args: str):
+    r = []
+    args = args.split(" ")
+    for arg in args:
+        if not arg:
+            pass
+        elif arg == "c" and cache_cards.get(event):
+            if cache_cards.delete(event, save=False):
+                r.append("已清空暂存人物卡数据")
+        elif arg == "card" and cards.get(event):
+            if cards.delete(event):
+                r.append("已删除使用中的人物卡！")
+        else:
+            if cards.delete_skill(event, arg):
+                r.append("已删除技能"+arg)
+    if not r:
+        r.append(help_messages.del_)
+    return r
+
+
+def sa_handler(event: MessageEvent, args: str):
+    if not args:
+        return help_messages.sa
+    elif not cards.get(event):
+        return "请先使用set指令保存人物卡后再使用快速检定功能。"
+    for attr, alias in attrs_dict.items():
+        if args in alias:
+            arg = alias[0]
+            break
+    card_data = cards.get(event)
+    dices = diro.parse("")
+    return expr(dices, card_data[arg])
