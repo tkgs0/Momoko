cd03f064a6b3636db1b89a7162d3583f4ba3f86b
tkgs0 <hgc.sakuya@ya.ru>
2023-12-30 05:18:47 +0000

    commit

--- basic_plugins/manager/__init__.py
+++ basic_plugins/manager/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from nonebot.plugin import PluginMetadata
+from nonebot.plugin import on_command, PluginMetadata
 from nonebot.adapters.onebot.v11 import MessageSegment
 from . import (
     bot_id,
@@ -11,12 +11,16 @@ from . import (
 )
 
 
-usage = f'{MessageSegment.image(Path(__file__).parent / "manager_help.png")}'
-
-
 __plugin_meta__ = PluginMetadata(
     name="管理",
     description="好友/群聊管理插件",
-    usage=usage,
+    usage="发送 manager.help 查看帮助",
     type="application"
 )
+
+
+m = on_command("mamager.help", priority=2, block=True)
+
+@m.handle()
+async def _():
+    await m.finish(MessageSegment.image(Path(__file__).parent / "manager_help.png", cache=False))
