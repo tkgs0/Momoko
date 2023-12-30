a75ed01c77610c47632b02aeb6f5d6923648e828
tkgs0 <hgc.sakuya@ya.ru>
2023-12-30 05:55:53 +0000

    commit

--- basic_plugins/manager/__init__.py
+++ basic_plugins/manager/__init__.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from nonebot.plugin import on_command, PluginMetadata
-from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11 import MessageSegment
 from . import (
     bot_id,
     group_manage,
@@ -15,14 +14,13 @@ from . import (
 __plugin_meta__ = PluginMetadata(
     name="管理",
     description="好友/群聊管理插件",
-    usage="发送 manager.help 查看帮助",
+    usage="发送 mhelp 查看帮助",
     type="application"
 )
 
 
-m = on_command("mamager", priority=2, block=False)
+m = on_command("mhelp", priority=2, block=False)
 
 @m.handle()
-async def _(args: Message = CommandArg()):
-    if args.extract_plain_text().strip() == ".help":
-        await m.finish(MessageSegment.image(Path(__file__).parent / "manager_help.png", cache=False))
+async def _():
+    await m.finish(MessageSegment.image(Path(__file__).parent / "manager_help.png", cache=False))
